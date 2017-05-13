45e057d8a2562dc558aad311ecea2ce36e4b58af
choi14 <jychoi@mwnl.snu.ac.kr>
2017-05-13 18:16:40 +0900

    jychoi

--- ubertooth-2015-10-R1/firmware/update_tx.sh
+++ ubertooth-2015-10-R1/firmware/update_tx.sh
@@ -10,7 +10,8 @@ while true;do
 	printf -v hCHAN "%x" "${CHAN}"
 	printf -v hUTIL "%x" "${UTIL}"
 	if [ ${READY} -eq 1 ]; then
-		sudo ubertooth-btle -s $1:$1:$1:$1:$1:$1:${hCHAN}:${hUTIL} -U $2		
+		sudo ubertooth-btle -s $1:aa:aa:aa:aa:aa:${hCHAN}:${hUTIL} -U $2		
+#sudo ubertooth-btle -s $1:$1:$1:$1:$1:$1:${hCHAN}:${hUTIL} -U $2		
 #sudo ubertooth-btle -s 74:de:2b:95:a0:1a:${hCHAN}:${hUTIL} -U 0		
 		echo ubertooth command ${UTIL}:${hUTIL} ${CHAN}:${hCHAN} ${READY}
 		echo 0 > /sys/kernel/debug/ieee80211/phy0/jychoi/chan_ready
